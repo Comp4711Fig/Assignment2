d9fb8d0d6ba584e04126f83174e98c3b9d513ee5
ljf920727 <ljf920727@gmail.com>
2017-04-01 22:29:56 -0700

    add the comments to assembly page and fix bug

--- application/controllers/Assembly.php
+++ application/controllers/Assembly.php
@@ -25,7 +25,7 @@ class Assembly extends Application
         $parts = $this->parts->all();
         //get all robots
         $robots = $this->robots->all();
-        //assembly the single parts
+        //single parts
         foreach ($parts as $part){
             if($part->piece == 1){
                 $top[]= $this->parser->parse('_singlePart', (array) $part, true);
@@ -53,27 +53,27 @@ class Assembly extends Application
             'cell_start' => '<td class="justOne">',
             'table_close' => '</table>'
         );
-        //set the parser configure and parameters
+        
         $this->table->set_template($template);
+        //finally generate the top part table
         $this->table->set_caption('Top Part');
         $rows = $this->table->make_columns($top, 3);
-        
-        
         $this->data['thetableTop'] = $this->table->generate($rows);
+        
+        //generate the torso part table
         $this->table->set_caption('Torso Part');
         $rows = $this->table->make_columns($torso, 3);
-        
-        
-        
         $this->data['thetableTorso'] = $this->table->generate($rows);
+        
+        //generate the bottom part table
         $this->table->set_caption('Bottom Part');
         $rows = $this->table->make_columns($bottom, 3);
         $tableHtml = $this->table->generate($rows); 
         $tableHtml .= $this->parser->parse('_assemblyReturn',[], true);
-        
         $this->data['thetableBottom'] = $tableHtml;
         $this->data['partFormAction'] = $isAuth ? '/assembly/assemblyOrReturn' : '#';
         
+        //generate the assembled robots table
         $this->table->set_caption('Assembled Robots');
         $rows = $this->table->make_columns($cellsForRobots, 3);
         $this->data['thetableRobots'] = $this->table->generate($rows);
@@ -81,11 +81,11 @@ class Assembly extends Application
         $this->data['pagebody'] = 'assembly';
         $this->render();
     }
-    
+    //for assembly button or retrun button
     public function assemblyOrReturn(){
         $role = $this->session->userdata('userrole');
         if ($role != ROLE_SUPERVISOR) redirect('/');
-       
+       //if click assembly button
         if(isset($_POST['assembly'])){
             $error_msgs = array();
             $partIds = array();
@@ -97,10 +97,11 @@ class Assembly extends Application
                  }
             }
            
-            
+            //validation
             if(count($partIds) != 3 ){
-                $error_msgs[]="You must choose exactly 3 parts to assembly a robot!";
+                $error_msgs[]="You must choose exactly 3 parts !";
             }
+            //get the id that choosed
             if(count($error_msgs) == 0){
                 $pieceIds = [1,2,3];
                 foreach ($partIds as $partId){
@@ -120,7 +121,7 @@ class Assembly extends Application
                     }
                  }
             }
-            
+            //create the robot table
             if (count($error_msgs) == 0) {
                 $robot = $this->robots->create();
                 $robot->part1CA = $orderedPartsIds[0];
@@ -129,12 +130,12 @@ class Assembly extends Application
                 $robot->timestamp = date('Y-m-d H:i:s',time());
                 $robot->status = 1;
                 $this->robots->add($robot);
-             
-             
+                
               $this->alert('Assembly a robot Successful!');
             }else{
                 $this->alert('<strong>Validation errors!<strong><br>'.$error_msgs[0], 'danger');
             }
+            //if click return button
          } else if(isset($_POST['return'])){
             $url = "http://umbrella.jlparry.com/work/recycle";
             $partsUrl = "";
@@ -145,24 +146,20 @@ class Assembly extends Application
                     $partIds[] = substr($key, 4);
                 }
             }
-            
+                
                 foreach( $partIds as $partId) {
                     $partsUrl .= "/".$partId;
+                    //check url is base_url/part1/part2/part3
                     if(substr_count($partsUrl, "/") == 3){
                         $allUrl = $url.$partsUrl;
                         $response = file_get_contents($allUrl.'?key=2fba4b');
-                     
+                        $responses= explode(" ", $response);
                         if ($response == "Ok"){
-                              echo "ok";   
+                            $gets+= $responses;
                         }
                         $partsUrl = "";
                     }
-                    
-             
-                    
-                  
-                    
-               
+               //delete the part for parts table
                $this->parts->delete($partId);   
                $this->alert('Return Parts Successful!');
         }
