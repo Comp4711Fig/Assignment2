540ee32675e0258220b7fcb372eefbf1482eb24f
Steven Tsao <steven.tsao777@gmail.com>
2017-04-02 15:28:04 -0700

    implemented basic sell robots functionality

--- application/views/manage.php
+++ application/views/manage.php
@@ -1,4 +1,20 @@
 
 <a href="/manage/reboot"><input type="button" value="Reboot"></a>
-<p></p>
 <a href="/manage/register"><input type="button" value="Register"></a>
+
+<h4>Built Robots</h4>
+<table class="table">
+    <tr>
+        <th>ID</th> 
+        <th>Part 1</th>
+        <th>Part 2</th>
+        <th>Part 3</th>
+        <th></th>
+    </tr>
+    {display_robots}
+</table>
+
+<form role="form" action="/manage/sellrobot" method="post">
+    {frobot}
+    {zsubmit}
+</form>
---
+++ application/views/onerobot.php
@@ -0,0 +1,8 @@
+<tr>
+    <td>{id}</td> 
+    <td>{part1CA}</td>
+    <td>{part2CA}</td>
+    <td>{part3CA}</td>
+    <td>
+    </td>
+</tr>
